84887dc628849ab1b965c3a88ab5b1c48b4744aa
DKMlL <86150988+DKMlL@users.noreply.github.com>
2022-04-08 16:20:28 +0800

    Update Operation.java
    Added Multiplication and division

--- hw5/Operation.java
+++ hw5/Operation.java
@@ -7,5 +7,16 @@ public class Operation {
         int c = a - b;
         return c;
     }
-    
+    static int mul(int a, int b){
+        int c = a * b;
+        return c;
+    }
+    static float div(float a,float b){
+        if(b == 0)
+            return 0;
+        else{
+            float c = a / b;
+            return c;
+        }
+    }
 }
